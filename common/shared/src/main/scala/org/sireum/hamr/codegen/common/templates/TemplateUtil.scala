56f4c4a74a3bd3604ca8b2ed6d1016277e324e1e
Jason Belt <belt@ksu.edu>
2021-02-26 19:23:43 +0000

    update submodules

---
+++ common/shared/src/main/scala/org/sireum/hamr/codegen/common/templates/TemplateUtil.scala
@@ -0,0 +1,11 @@
+// #Sireum
+
+package org.sireum.hamr.codegen.common.templates
+
+import org.sireum._
+
+object TemplateUtil {
+  def uniqueSTs(sts: ISZ[ST]): ISZ[ST] = {
+    return (Set.empty[String] ++ (sts.map((x: ST) => x.render))).elements.map((s: String) => st"$s")
+  }
+}
