ec10a98fdf92f7fecc512a09e90dc223929fbf96
Jason Belt <belt@ksu.edu>
2021-07-20 16:39:01 +0000

    slash transpile

--- common/shared/src/main/scala/org/sireum/hamr/codegen/common/templates/TemplateUtil.scala
+++ common/shared/src/main/scala/org/sireum/hamr/codegen/common/templates/TemplateUtil.scala
@@ -5,6 +5,7 @@ package org.sireum.hamr.codegen.common.templates
 import org.sireum._
 
 object TemplateUtil {
+
   def uniqueSTs(sts: ISZ[ST]): ISZ[ST] = {
     return (Set.empty[String] ++ (sts.map((x: ST) => x.render))).elements.map((s: String) => st"$s")
   }
