f11ebb8ae0b356457dea87c218b7331f0fb49219
Jason Belt <belt@ksu.edu>
2023-04-26 13:35:23 -0500

    slang check integration

--- common/shared/src/main/scala/org/sireum/hamr/codegen/common/containers/Containers.scala
+++ common/shared/src/main/scala/org/sireum/hamr/codegen/common/containers/Containers.scala
@@ -79,5 +79,4 @@ import org.sireum._
                                 val versions: ISZ[String],
                                 val cache: Option[String],
                                 val docs: B,
-                                val sources: B,
-                                val repositories: ISZ[String])
+                                val sources: B,                                val repositories: ISZ[String])
