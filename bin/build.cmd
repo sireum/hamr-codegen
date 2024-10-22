c90de333777baecf5edfaf4b89533340e24b5d43
Jason Belt <belt@ksu.edu>
2024-10-22 10:08:21 -0500

    remove camkes z3 version mod

--- bin/build.cmd
+++ bin/build.cmd
@@ -322,15 +322,6 @@ def installSbtMill(): Unit = {
   }
 }
 
-Os.env("GITHUB_WORKFLOW") match {
-  case Some(n) if (ops.StringOps(ops.StringOps(n).toLower).contains("camkes")) =>
-    val ver = (home / "versions.properties").properties.get("org.sireum.version.z3").get
-    println(s"z3 4.12+ requires glibc 2.35 but the camkes container has 2.31 so installing z3 4.11.2 rather than ${ver}")
-    proc"sed -i s/org.sireum.version.z3=${ver}/org.sireum.version.z3=4.11.2/g ${home / "versions.properties"}".runCheck()
-    proc"cat ${home / "versions.properties"}".console.runCheck()
-  case _ =>
-}
-
 var continue = T
 for (i <- 0 until Os.cliArgs.size if continue) {
   Os.cliArgs(i) match {
